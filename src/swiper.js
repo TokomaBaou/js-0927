c01a9fb806958c0596a1ce76be71db5e64489b0a
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-09-27 17:16:03 +0900

    [add]autoplay 自動再生機能の追加

--- src/swiper.js
+++ src/swiper.js
@@ -7,5 +7,8 @@ export let mySwiper = new Swiper(".swiper-container", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
+  },
+  autoplay: {
+    delay: 5000
   }
 });
